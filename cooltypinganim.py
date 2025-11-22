3fc42ad1536c228cad0f60fdf0495028a6e6c0cf
SoNotClose <166761762+SoNotClose@users.noreply.github.com>
2025-11-22 00:24:05 -0500

    i wanna remake this but in c#
    2yrs old btw

--- cooltypinganim.py
+++ cooltypinganim.py
@@ -1,1 +1,25 @@
-# nothing here
+import time
+import sys
+
+whattotype = "c# is better tjan this lang"
+typingspeed = 0.1 
+# randomizespeed = false
+indicator = "|"  
+
+for char in whattotype:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(typingspeed)
+    sys.stdout.write(indicator)
+    sys.stdout.flush()
+    time.sleep(typingspeed / 2)
+    sys.stdout.write("\b \b")
+    sys.stdout.flush()
+
+while True:
+    sys.stdout.write(indicator)
+    sys.stdout.flush()
+    time.sleep(0.5)
+    sys.stdout.write("\b \b")
+    sys.stdout.flush()
+    time.sleep(0.5)
